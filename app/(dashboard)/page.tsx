3c12c8c8c092c3d3c3a4b2bd663aa757c94fd4f0
Guilherme Tessarini <guilherme.tessarini@hotmail.com>
2024-07-23 14:42:28 -0300

    feat: implement GET and POST endpoints for accounts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+
 export default function Home() {
+
+  const { onOpen } = useNewAccount();
+
   return (
-    <p>Dashboard Page</p>
+    <Button onClick={onOpen}>
+      Ad an account
+    </Button>
   );
 }
